a30a9215fdcb526aaf56b621ff6570421595d27e
llaa33219 <lukems903@gmail.com>
2026-04-18 19:37:30 +0900

    Bump version to 0.1.3

--- src/outowiki/__init__.py
+++ src/outowiki/__init__.py
@@ -37,7 +37,7 @@ from .modules.searcher import Searcher
 from .modules.agent import InternalAgent
 from .providers.base import LLMProvider
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 
 def __getattr__(name: str) -> type:
