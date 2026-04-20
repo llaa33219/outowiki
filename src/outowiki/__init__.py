876738a515a5acecfe6f5f6a5bdf7fe0ed307fdf
llaa33219 <lukems903@gmail.com>
2026-04-20 18:51:11 +0900

    chore: bump version to 0.5.2

--- src/outowiki/__init__.py
+++ src/outowiki/__init__.py
@@ -37,7 +37,7 @@ from .modules.searcher import Searcher
 from .modules.agent import InternalAgent
 from .providers.base import LLMProvider
 
-__version__ = "0.5.0"
+__version__ = "0.5.2"
 
 
 def __getattr__(name: str) -> type:
