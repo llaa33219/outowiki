3893c1ef9cef5824189af08556694b69ff553dfb
llaa33219 <lukems903@gmail.com>
2026-04-26 15:41:33 +0900

    chore: bump version to 0.6.4
    Feat: Emphasize intuitive titles distinct from file paths

--- src/outowiki/__init__.py
+++ src/outowiki/__init__.py
@@ -37,7 +37,7 @@ from .modules.searcher import Searcher
 from .modules.agent import InternalAgent
 from .providers.base import LLMProvider
 
-__version__ = "0.6.3"
+__version__ = "0.6.4"
 
 
 def __getattr__(name: str) -> type:
